82673bd27fd6ca67b5ff95878169f311cb7be44c
VladimirJG <vladimi85@gmail.com>
2023-06-07 09:09:34 +0300

    Fixed: count without initialization, call directly; fill(to index without -1); save by size.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,25 +5,20 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int count = 0;
+    private int count;
 
     void clear() {
-        Arrays.fill(storage, 0, size() - 1, null);
+        Arrays.fill(storage, 0, count, null);
         count = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                count++;
-                break;
-            }
-        }
+        storage[size()] = r;
+        count++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < count - 1; i++) {
             if (storage[i].toString().equals(uuid)) {
                 return storage[i];
             }
@@ -32,10 +27,10 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size() - 1; i++) {
+        for (int i = 0; i < count - 1; i++) {
             if (storage[i].toString().equals(uuid)) {
-                storage[i] = storage[size() - 1];
-                storage[size() - 1] = null;
+                storage[i] = storage[count - 1];
+                storage[count - 1] = null;
                 count--;
                 break;
             }
@@ -46,7 +41,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
