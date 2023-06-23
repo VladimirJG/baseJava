12faced5f9f9897c5bb06e223f778b531333e5e8
VladimirJG <vladimi85@gmail.com>
2023-06-23 08:20:29 +0300

    Written MapStorage class;

---
+++ src/com/basejava/webapp/storage/MapStorage.java
@@ -0,0 +1,57 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+    Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get((String) searchKey);
+    }
+
+    @Override
+    protected void doSave(Object searchKey, Resume resume) {
+        storage.put((String) searchKey, resume);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove((String) searchKey);
+    }
+
+    @Override
+    protected void doUpdate(Object searchKey, Resume resume) {
+        storage.replace((String) searchKey, resume);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return storage.containsKey((String) searchKey);
+    }
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        Resume[] newValueArray = new Resume[storage.size()];
+        storage.values().toArray(newValueArray);
+        return newValueArray;
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
