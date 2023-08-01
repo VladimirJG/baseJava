b63c8b268042b1580ac5c01ad5f2e4021e4d9e12
VladimirJG <vladimi85@gmail.com>
2023-08-01 16:27:44 +0300

    CompanySection, ListSection, TextSection added equals, hashCode, toString

--- src/com/basejava/webapp/model/CompanySection.java
+++ src/com/basejava/webapp/model/CompanySection.java
@@ -1,5 +1,58 @@
 package com.basejava.webapp.model;
 
-public class CompanySection extends AbstractSection {
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
+public class CompanySection extends AbstractSection {
+    private final String nameCompany;
+    private final String jobTitle;
+    private final Date workPeriod;
+    private final List<String> jobDescription;
+
+    public CompanySection(String nameCompany, String jobTitle, Date workPeriod, List<String> jobDescription) {
+        this.nameCompany = nameCompany;
+        this.jobTitle = jobTitle;
+        this.workPeriod = workPeriod;
+        this.jobDescription = jobDescription;
+    }
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public Date getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public List<String> getJobDescription() {
+        return jobDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "CompanySection{" +
+                "nameCompany='" + nameCompany + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", workPeriod=" + workPeriod +
+                ", jobDescription=" + jobDescription +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CompanySection that = (CompanySection) o;
+        return Objects.equals(nameCompany, that.nameCompany) && Objects.equals(jobTitle, that.jobTitle) && Objects.equals(workPeriod, that.workPeriod) && Objects.equals(jobDescription, that.jobDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameCompany, jobTitle, workPeriod, jobDescription);
+    }
 }
--- src/com/basejava/webapp/model/ListSection.java
+++ src/com/basejava/webapp/model/ListSection.java
@@ -1,6 +1,7 @@
 package com.basejava.webapp.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ListSection extends AbstractSection {
     private final List<String> listOfTexts;
@@ -11,5 +12,25 @@ public class ListSection extends AbstractSection {
 
     public List<String> getListOfTexts() {
         return listOfTexts;
+    }
+
+    @Override
+    public String toString() {
+        return "ListSection{" +
+                "listOfTexts=" + listOfTexts +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListSection that = (ListSection) o;
+        return Objects.equals(listOfTexts, that.listOfTexts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listOfTexts);
     }
 }
