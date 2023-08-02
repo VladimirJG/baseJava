7a97155acfe02a9297e1af8ece921f750d0a8d29
VladimirJG <vladimi85@gmail.com>
2023-08-02 07:44:16 +0300

    Added Link class

--- src/com/basejava/webapp/model/CompanySection.java
+++ src/com/basejava/webapp/model/CompanySection.java
@@ -14,4 +14,24 @@ public class CompanySection extends AbstractSection {
     public List<Company> getCompanies() {
         return companies;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CompanySection that = (CompanySection) o;
+        return Objects.equals(companies, that.companies);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(companies);
+    }
+
+    @Override
+    public String toString() {
+        return "CompanySection{" +
+                "companies=" + companies +
+                '}';
+    }
 }
