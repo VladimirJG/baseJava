0940648e1419360ea3d04d78ed76c63e11bbba64
VladimirJG <vladimi85@gmail.com>
2023-08-03 16:10:37 +0300

    Modified AbstractFileStorage class

--- src/com/basejava/webapp/storage/AbstractFileStorage.java
+++ src/com/basejava/webapp/storage/AbstractFileStorage.java
@@ -1,9 +1,11 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractFileStorage extends AbstractStorage<File> {
@@ -22,47 +24,79 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
 
     @Override
     protected Resume doGet(File searchKey) {
-        return null;
+        try {
+            return doRead(searchKey);
+        } catch (IOException e) {
+            throw new StorageException("File read error", searchKey.getName(), e);
+        }
     }
 
     @Override
     protected void doSave(File searchKey, Resume resume) {
-
+        try {
+            searchKey.createNewFile();
+        } catch (IOException e) {
+            throw new StorageException("Couldn't create file " + searchKey.getAbsolutePath(), searchKey.getName(), e);
+        }
+        doUpdate(searchKey, resume);
     }
 
     @Override
     protected void doDelete(File searchKey) {
-
+        if (!searchKey.delete()) {
+            throw new StorageException("File delete error", searchKey.getName());
+        }
     }
 
     @Override
     protected void doUpdate(File searchKey, Resume resume) {
-
+        try {
+            doWrite(resume, searchKey);
+        } catch (IOException e) {
+            throw new StorageException("File write error", resume.getUuid(), e);
+        }
     }
 
     @Override
     protected boolean isExist(File searchKey) {
-        return false;
+        return searchKey.exists();
     }
 
     @Override
     protected File getSearchKey(String key) {
-        return null;
+        return new File(directory, key);
     }
 
     @Override
     protected List<Resume> doGetCopyList() {
-        return null;
+        File[] files = getList(directory);
+        List<Resume> list = new ArrayList<>(files.length);
+        for (File file : files) {
+            list.add(doGet(file));
+        }
+        return list;
     }
 
     @Override
     public void clear() {
-
+        File[] files = getList(directory);
+        for (File file : files) {
+            doDelete(file);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return getList(directory).length;
     }
+
     protected abstract void doWrite(Resume resume, File searchKey) throws IOException;
+
+    protected abstract Resume doRead(File searchKey) throws IOException;
+
+    private File[] getList(File directory) {
+        File[] files = directory.listFiles();
+        assert files != null;
+        return files;
+    }
 }
