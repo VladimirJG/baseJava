e859aeefe6e4fae89c0c1f1db1dd812e83ac7576
VladimirJG <vladimi85@gmail.com>
2023-08-09 16:45:35 +0300

    Added XmlStreamSerializer.class;

---
+++ src/com/basejava/webapp/storage/serializer/XmlStreamSerializer.java
@@ -0,0 +1,30 @@
+package com.basejava.webapp.storage.serializer;
+
+import com.basejava.webapp.model.*;
+import com.basejava.webapp.util.XmlParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class XmlStreamSerializer implements StreamSerializer {
+    private XmlParser xmlParser;
+
+    public XmlStreamSerializer() {
+        xmlParser = new XmlParser(Resume.class, Company.class, Link.class, CompanySection.class,
+                TextSection.class, ListSection.class, Company.Position.class);
+    }
+
+    @Override
+    public void doWrite(Resume r, OutputStream os) throws IOException {
+        try (Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+            xmlParser.marshall(r, w);
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            return xmlParser.unmarshall(r);
+        }
+    }
+}
