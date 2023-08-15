e4886e368cd5dcc240bde994f1a3fc6fc5e2e427
VladimirJG <vladimi85@gmail.com>
2023-08-15 14:52:13 +0300

    Added MainConcurrency(Thread)

---
+++ src/com/basejava/webapp/MainConcurrency.java
@@ -0,0 +1,51 @@
+package com.basejava.webapp;
+
+public class MainConcurrency {
+    private static int counter;
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+       /* new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+            }
+        }.start();*/
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+            }
+        };
+        thread0.start();
+
+       /* new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        }).start();*/
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + Thread.currentThread().getState())).start();
+        System.out.println(thread0.getState());
+
+
+        for (int i = 0; i < 10000; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    inc();
+                }
+            }).start();
+        }
+        Thread.sleep(500);
+        System.out.println(counter);
+    }
+
+    private static void inc() {
+        double a = Math.sin(13.);
+        synchronized (LOCK) {
+            counter++;
+        }
+    }
+}
