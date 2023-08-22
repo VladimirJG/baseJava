407c1b1ee88d4dccd6b6a79edf1502e5c10cf811
VladimirJG <vladimi85@gmail.com>
2023-08-22 06:09:45 +0300

    Create SqlHelper,SqlExecutor, ExceptionUtil; modified SqlStorage.

--- test/com/basejava/webapp/storage/SqlStorageTest.java
+++ test/com/basejava/webapp/storage/SqlStorageTest.java
@@ -2,8 +2,6 @@ package com.basejava.webapp.storage;
 
 import com.basejava.webapp.Config;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SqlStorageTest extends AbstractStorageTest {
 
     protected SqlStorageTest() {
