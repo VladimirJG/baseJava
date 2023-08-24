73ac6a3b88a0233c1f7bdd1d732459c436041a5b
VladimirJG <vladimi85@gmail.com>
2023-08-24 10:22:35 +0300

    Added initialization from AbstractStorageTest in static block to ResumeTestData

--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -11,29 +11,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.List;
-import java.util.UUID;
+
+import static com.basejava.webapp.util.ResumeTestData.*;
 
 abstract class AbstractStorageTest {
     protected static final File STORAGE_DIR = Config.get().getStorageDir();
 
     protected Storage storage;
-    private final String UUID_1 = UUID.randomUUID().toString();
-    private final String UUID_2 = UUID.randomUUID().toString();
-    private final String UUID_3 = UUID.randomUUID().toString();
-    private final String UUID_4 = UUID.randomUUID().toString();
-    private final String FULL_NAME_1 = "Name1";
-    private final String FULL_NAME_2 = "Name2";
-    private final String FULL_NAME_3 = "Name3";
-    private final String FULL_NAME_4 = "Name4";
-    /*private final Resume RESUME_1 = ResumeTestData.createResume(UUID_1, FULL_NAME_1);
-    private final Resume RESUME_2 = ResumeTestData.createResume(UUID_2, FULL_NAME_2);
-    private final Resume RESUME_3 = ResumeTestData.createResume(UUID_3, FULL_NAME_3);
-    private final Resume RESUME_4 = ResumeTestData.createResume(UUID_4, FULL_NAME_4);*/
-    private final Resume RESUME_1 = new Resume(UUID_1, FULL_NAME_1);
-    private final Resume RESUME_2 = new Resume(UUID_2, FULL_NAME_2);
-    private final Resume RESUME_3 = new Resume(UUID_3, FULL_NAME_3);
-    private final Resume RESUME_4 = new Resume(UUID_4, FULL_NAME_4);
-    private final String UUID_NOT_EXIST = "dummy";
+
 
     protected AbstractStorageTest(Storage storage) {
         this.storage = storage;
--- test/com/basejava/webapp/util/ResumeTestData.java
+++ test/com/basejava/webapp/util/ResumeTestData.java
@@ -1,16 +1,43 @@
-package com.basejava.webapp;
+package com.basejava.webapp.util;
 
-import com.basejava.webapp.model.ContactType;
-import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.model.*;
+
+import java.time.Month;
+import java.util.UUID;
 
 
 public class ResumeTestData {
+    public static final String UUID_1 = UUID.randomUUID().toString();
+    public static final String UUID_2 = UUID.randomUUID().toString();
+    public static final String UUID_3 = UUID.randomUUID().toString();
+    public static final String UUID_4 = UUID.randomUUID().toString();
+    public static final String FULL_NAME_1 = "Name1";
+    public static final String FULL_NAME_2 = "Name2";
+    public static final String FULL_NAME_3 = "Name3";
+    public static final String FULL_NAME_4 = "Name4";
+    public static final Resume RESUME_1;
+    public static final Resume RESUME_2;
+    public static final Resume RESUME_3;
+    public static final Resume RESUME_4;
+    public static final String UUID_NOT_EXIST;
+
+    /*private final Resume RESUME_1 = ResumeTestData.createResume(UUID_1, FULL_NAME_1);
+    private final Resume RESUME_2 = ResumeTestData.createResume(UUID_2, FULL_NAME_2);
+    private final Resume RESUME_3 = ResumeTestData.createResume(UUID_3, FULL_NAME_3);
+    private final Resume RESUME_4 = ResumeTestData.createResume(UUID_4, FULL_NAME_4);*/
+    static {
+        RESUME_1 = ResumeTestData.createResume(UUID_1, FULL_NAME_1);
+        RESUME_2 = ResumeTestData.createResume(UUID_2, FULL_NAME_2);
+        RESUME_3 = ResumeTestData.createResume(UUID_3, FULL_NAME_3);
+        RESUME_4 = ResumeTestData.createResume(UUID_4, FULL_NAME_4);
+        UUID_NOT_EXIST = "dummy";
+    }
 
     public static Resume createResume(String uuid, String fullName) {
         Resume resume = new Resume(uuid, fullName);
         resume.addContacts(ContactType.TELEPHONE, "1544-1544");
         resume.addContacts(ContactType.MAIL, "@mail.com");
-       /* resume.addContacts(ContactType.LINKEDIN, "InDinLink");
+        resume.addContacts(ContactType.LINKEDIN, "InDinLink");
         resume.addContacts(ContactType.HOMEPAGE, "tratata@55.ru");
         resume.addContacts(ContactType.SKYPE, "565897444");
         resume.addContacts(ContactType.GITHUB, "HOB");
@@ -26,12 +53,12 @@ public class ResumeTestData {
                 new Company("Organization12", "http://Organization12.ru")));
         resume.addSections(SectionType.ACHIEVEMENT, new ListSection("Ate 7 cutlets in 5 minutes", "Wrote several standalone programs"));
         resume.addSections(SectionType.OBJECTIVE, new TextSection("Programmer"));
-        resume.addSections(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));*/
+        resume.addSections(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
 
         return resume;
     }
 
-    public static Resume createRII(String uuid, String fullName){
+    public static Resume createRII(String uuid, String fullName) {
         Resume resume = new Resume(uuid, fullName);
         resume.addContacts(ContactType.SKYPE, "565897444");
         resume.addContacts(ContactType.GITHUB, "HOB");
