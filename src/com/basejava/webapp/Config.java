6fb4a021cdfc2e976ade43a3c09cbacbaa58610e
VladimirJG <vladimi85@gmail.com>
2023-08-30 15:26:55 +0300

    refactoring Config;

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -4,6 +4,7 @@ import com.basejava.webapp.storage.SqlStorage;
 import com.basejava.webapp.storage.Storage;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
@@ -11,27 +12,27 @@ import java.util.Properties;
 import java.util.Set;
 
 public class Config {
-    private static final String PROPS = "config\\resumes.properties";
+    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
     private static final Config INSTANCE = new Config();
+    /*private final Properties props = new Properties();*/
     private final File storageDir;
     private final Storage storage;
     private Set<String> immutableUuids = new HashSet<>() {{
         add("11111111-1111-1111-1111-111111111111");
         add("22222222-2222-2222-2222-222222222222");
     }};
-
     public static Config get() {
         return INSTANCE;
     }
 
     private Config() {
-        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+        try (InputStream is = new FileInputStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS);
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
     }
 
@@ -42,7 +43,14 @@ public class Config {
     public Storage getStorage() {
         return storage;
     }
-
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
+    }
     public boolean isImmutable(String uuids) {
         return immutableUuids.contains(uuids);
     }
